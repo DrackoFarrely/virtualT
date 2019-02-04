e11abca11a62cd9048dfbd5775525a89780398b6
DrackoFarrely <vi_c_tor01@hotmail.com>
2019-02-04 15:02:26 -0600

    Add files via upload

---
+++ UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Virtual_T.Business;
+using Virtual_T.Models.Dominio;
+
+namespace Virtual_T.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        UsuarioService usuarioService;
+        //Inyeccion de dependencias
+        public UsuarioController() : this(new UsuarioService()) { }
+        public UsuarioController(UsuarioService _usuarioService)
+        {
+            this.usuarioService = _usuarioService;
+        }
+        //Acciones (Métodos) de Routeo
+        public ActionResult Listar()
+        {
+            List<Usuario> lista = usuarioService.ListarTodo();
+            if (lista != null)
+                return View(lista);
+            ViewBag.Error = "Error al traer la lista";
+            return View(new List<Usuario>());
+        }
+       
+        public ActionResult Create()
+        {
+            Usuario entity = new Usuario();
+            return View(entity);
+
+        }
+        [HttpPost]
+        public ActionResult Create(Usuario entity)
+        {
+            usuarioService.Guardar(entity);
+            return RedirectToAction("Listar");
+
+        }
+
+    }
+}
